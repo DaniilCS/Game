c7115b785766ac3f81e2e6c41a7b7ff1dab9b08b
Даниил Бураков <diburakov@edu.hse.ru>
2020-06-03 16:39:47 +0300

    Код документирован и в тексте программы и в отдельном файле описана структура данных и операции над ней.

--- Game/Field.h
+++ Game/Field.h
@@ -6,33 +6,55 @@
 
 using namespace std;
 
+/* Name of the data structure: Field, which based on square matrix.
+   Cells of this matrix can take values of powers of two from 2 to 1024.
+   This data structure is intended for initializing 1024 game fields.
+
+   Main Operations:
+   1) RndCell. Gives one random field cell the value 2 or 4. Value 2 with probability 90% and 4 with 10%.
+   2) PushRight. Moves nonzero values of matrix to the right. If value of the cell equals to the value of right cell, then right cell
+   will have value 2*Old Value. 
+   3) PushLeft. Moves nonzero value of matrix to the left. If value of the cell equals to the value of right cell, then right cell
+   will have value 2*Old Value.
+   4) Transposition. Only transpose matrix of field.
+   5)PushUp. Moves nonzero value of matrix up. If value of the cell equals to the value of right cell, then right cell
+   will have value 2*Old Value.
+   6)PushDown. Moves nonzero value of matrix down. If value of the cell equals to the value of right cell, then right cell
+   will have value 2*Old Value.
+   7)CheckGameOver. Check if game fiel change after using PushRight, PushLeft, PushUp, PushDown. If field doesn't change returns false, else true.
+   8)CheckWin. Check if some cell of matrix has value 1024. If has then game is passed, else not.
+*/
+
 class Field
 {
 	public:
-		 vector<vector<int>> field;
+		 vector<vector<int>> field;// matrix from vectors from library <vector>
 
+		 /*Function for genering random values from between min and max-1.*/
 		 int Random(int min, int max) {
 			 return min + rand() % (max - min);
 		 }
 
+		 /*Initialise constuctor of class Field*/
 		 Field(int range) {
-			 field.resize(range);
+			 field.resize(range);//resize vactor of vectors for range
 
 			 for (int i = 0; i < range; i++) {
-				 field[i].resize(range);
+				 field[i].resize(range);// riesize every vector in vector for range
 				 for (int j = 0; j < range; j++) {
 					 field[i][j] = 0;
-				 }
+				 }//Initialise every cell of matrix with zeros.
 			 }
 		 }
 
+		 /* Gives random cell od matrix value 2 with probability 90% and 4 with probability 10%*/
 		void RndCell() 
 		{
 			bool flag = true;
 			for (int i = 0; i < field.size(); i++)
 				for (int j = 0; j < field.size(); j++)
 					if (field[i][j] == 0)
-						flag = false;
+						flag = false;// seak for empty cell of matrix. If there is no free cell end this function.
 
 			if (flag)
 				return;
@@ -43,16 +65,17 @@ class Field
 			while (field[icord][jcord] != 0) {
 				icord = Random(0, field.size());
 				jcord = Random(0, field.size());
-			}
+			}//genering random coordinates of matrix cell
 
 			if (Random(1, 11) != 1) {
 				field[icord][jcord] = 2;
 			}
 			else {
 				field[icord][jcord] = 4;
-			}
+			}//place random value on this cell
 		}
 
+		/*Push all nonzero values to the right*/
 		void PushRight(){
 			int nowI;
 			int nowJ;
@@ -65,16 +88,19 @@ class Field
 							field[nowI][nowJ + 1] = field[nowI][nowJ];
 							field[nowI][nowJ] = 0;
 							nowJ++;
-						}
+						}//moves the values of matrix cells to the right until it meets 
+						//the end of the field or a cell with a different value
+
 						if ((nowJ != field.size() - 1) && (field[nowI][nowJ] == field[nowI][nowJ + 1])) {
 							field[nowI][nowJ + 1] *=2;
 							field[nowI][nowJ] = 0;
-						}
+						}//if a cell with the same value is found it merges them into one with the value 2*OldValue
 					}
 				}
 			}
 		}
 
+		/*Push all nonzero values to the left*/
 		void PushLeft() {
 			int nowI;
 			int nowJ;
@@ -87,16 +113,18 @@ class Field
 							field[nowI][nowJ - 1] = field[nowI][nowJ];
 							field[nowI][nowJ] = 0;
 							nowJ--;
-						}
+						}//moves the values of matrix cells to the left until it meets 
+						//the end of the field or a cell with a different value
 						if ((nowJ != 0) && (field[nowI][nowJ] == field[nowI][nowJ - 1])) {
 							field[nowI][nowJ - 1] *= 2;
 							field[nowI][nowJ] = 0;
-						}
+						}//if a cell with the same value is found it merges them into one with the value 2*OldValue
 					}
 				}
 			}
 		}
 
+		/*Transose matrix. Used in PushUp and PushDown to use in them PushLeft and PushDown*/
 		void Transpozition() {
 			int swap;
 			for (int i = 0; i < field.size(); i++) {
@@ -108,18 +136,22 @@ class Field
 			}
 		}
 
+		/*Push all nonzero values up*/
 		void PushUp() {
 			Transpozition();
 			PushLeft();
 			Transpozition();
-		}
+		}//using transpose we can use PushLeft and again transpose and get result of PushUp
 
+		/*Push all nonzero values up*/
 		void PushDown() {
 			Transpozition();
 			PushRight();
 			Transpozition();
-		}
+		}//using transpose we can use PushRight and again transpose and get result of PushDown
 
+		/*Searches for cells that are connected horizontally or vertically with the same value. If there is no
+		such cell, game over.*/
 		bool CheckGameOver() {
 			for (int i = 0; i < field.size(); i++) {
 				for (int j = 0; j < field.size(); j++) {
@@ -139,6 +171,7 @@ class Field
 			return false;
 		}
 
+		/*Search cell of matrix with 1024 value. If true then user win the game.*/
 		bool CheckWin() {
 			for (int i = 0; i < field.size(); i++)
 				for (int j = 0; j < field.size(); j++) {
--- Game/Game.cpp
+++ Game/Game.cpp
@@ -6,24 +6,30 @@
 
 using namespace std;
 
+/*Draw graffic field with library SFML according to data structure Field.*/
 void DrawField(sf::RenderWindow& window, Field& field, sf::Font& font, string path) {
 	for (int j = 0; j < field.field.size(); j++) {
 		for (int i = 0; i < field.field.size(); i++)
 		{
 			sf::Texture tex;
 			sf::Sprite sprite;
+			//if value is nonzero then load texture from file
 			if (field.field[i][j] != 0) {
 				int number = field.field[i][j];
 				tex.loadFromFile(path + (to_string(number))+".jpg");
 				sprite.setTexture(tex);
 				sprite.move(60 + 80 * j, 60 + 78 * i);
 			}
+
+			//draw yellow squre on place with cell with zero value and texture if cell's value nonzero
 			sf::Color color = sf::Color(128, 128, 128);
 			sf::RectangleShape shape(sf::Vector2f(75, 75));
 			shape.move(60 + 80 * j, 60 + 78 * i);
 			shape.setOutlineThickness(2.f);
 			shape.setOutlineColor(color);
 			shape.setFillColor(sf::Color::Yellow);
+
+			//place texture of square filled yellow collor or texture
 			window.draw(shape);
 			if (field.field[i][j] != 0) {
 				window.draw(sprite);
@@ -32,41 +38,45 @@ void DrawField(sf::RenderWindow& window, Field& field, sf::Font& font, string pa
 	}
 }
 
+/*function with saving game field to file*/
 void Save(Field& f, string path) {
 	ofstream rec;
-	rec.open(path + "save.txt");
+	rec.open(path + "save.txt");//oppening stream
 	if (rec.is_open()) {
 		rec << f.field.size() << endl;
 		for (int i = 0; i < f.field.size(); i++)
 			for (int j = 0; j < f.field.size(); j++)
-				rec << f.field[i][j] << endl;
+				rec << f.field[i][j] << endl;//recording every cell of field to txt file from new line
 	}
-	rec.close();
+	rec.close();//close stream
 }
 
+/*function that load cells field from txt file*/
 bool Load(string path, Field& f) {
-	ifstream read(path + "save.txt");
+	ifstream read(path + "save.txt");//opening stream
 	string line;
 	if (read.is_open()) {
 		if (getline(read, line)) {
-			f = Field(atoi(line.c_str()));
+			f = Field(atoi(line.c_str()));//read the first value with range of matrix
 		}
 		else {
-			return false;
+			read.close();
+			return false;//if file is empty close stream
 		}
 
 		for (int i = 0; i < f.field.size(); i++)
 			for (int j = 0; j < f.field.size(); j++) {
-				getline(read, line);
-				f.field[i][j] = atoi(line.c_str());
+				getline(read, line);// read lines of file
+				f.field[i][j] = atoi(line.c_str());//convert values of cells from string to int
 			}	
 	}
-	read.close();
-	return true;
+	read.close();//close stream
+	return true;// return true, loading is correct
 }
 
 int main(int argc, char** argv)
 {
+	/*Search path directory with exe and textures*/
 	string path1 = "";
 	int count = 0;
 	for (int i = 0; i < argc; ++i) {
@@ -85,6 +95,7 @@ int main(int argc, char** argv)
 		}
 	}
 
+	//Initialise window with menu of game
 	sf::RenderWindow window(sf::VideoMode(600, 600), "1024");
 	window.setFramerateLimit(60);
 
@@ -94,6 +105,7 @@ int main(int argc, char** argv)
 	sf::Sprite sprite;
 	Field f = Field(0);
 
+	//load texture of menu and font for writing
 	if (!font.loadFromFile(path + "arial.ttf"))
 	{
 		// Error
@@ -104,16 +116,16 @@ int main(int argc, char** argv)
 	}
 	sprite.setTexture(texture);
 
-
+	//open main menu with selecting range of field and loading game
 	while (window.isOpen())
 	{
-		window.clear();
+		window.clear();//upload window
 		
 		sprite.setPosition(sf::Vector2f(0, 0));
 		sprite.setScale(sf::Vector2f(1.f, 2.f));
-		window.draw(sprite);
+		window.draw(sprite);//draw main menu texture
 
-		sf::Text text;
+		sf::Text text;//draw text of main menu
 		text.setFont(font);
 		text.setString(L"Chose Field For Game");
 		text.setCharacterSize(30);
@@ -121,106 +133,109 @@ int main(int argc, char** argv)
 		text.move(140, 150);
 		window.draw(text);
 
-		text.setString(L"Field 4 x 4 \nPress F4");
+		text.setString(L"Field 4 x 4 \nPress F4");//for field 4 x 4 push F4
 		text.move(-110, 300);
 		window.draw(text);
 
 
-		text.setString(L"Field 5 x 5 \nPress F5");
+		text.setString(L"Field 5 x 5 \nPress F5");//for field 5 x 5 push F5
 		text.move(190, 0);
 		window.draw(text);
 
-		text.setString(L"Field 6 x 6 \nPress F6");
+		text.setString(L"Field 6 x 6 \nPress F6");//for field 6 x 6 push F6
 		text.move(190, 0);
 		window.draw(text);
 
-		text.setString(L"Press L to Load Level");
+		text.setString(L"Press L to Load Level");//to load field press L
 		text.move(-270, 100);
 		window.draw(text);
 
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed) 
+			if (event.type == sf::Event::Closed) //closing window on closed button
 			{
 				window.close();
 				return 0;
 			}
 			if (event.type == sf::Event::KeyPressed)
 			{
-				if (event.key.code == sf::Keyboard::Escape) window.close();
-				if (event.key.code == sf::Keyboard::F4) {
+				if (event.key.code == sf::Keyboard::Escape) window.close();//close window on press ESSC
+				if (event.key.code == sf::Keyboard::F4) {//Initialise field 4 x 4 on pressing F4
 					f = Field(4); window.close();
 				}
-				if (event.key.code == sf::Keyboard::F6) {
+				if (event.key.code == sf::Keyboard::F6) {//Initialise field 6 x 6 on pressing F6
 					f = Field(6); window.close();
 				}
-				if (event.key.code == sf::Keyboard::F5) {
+				if (event.key.code == sf::Keyboard::F5) {//Initialise field 5 x 5 on pressing F5
 					f = Field(5); window.close();
 				}
-				if (event.key.code == sf::Keyboard::L) {
+				if (event.key.code == sf::Keyboard::L) {//Load field from file on pressing L
 					if (Load(path, f)) {
 						window.close();
 					}
 				}
 			}
 		}
-		window.display();
+		window.display();//upload window
 	}
 	
+	//new window 
 	sf::RenderWindow window1(sf::VideoMode(60 + 80 * f.field.size() + 60, 60 + 78 * f.field.size() + 60), "1024");
 	window1.setFramerateLimit(60);
 
+	//open window with game while game is going
 	while (window1.isOpen() && (f.field.size() != 0) && (f.CheckGameOver()) && (!f.CheckWin()))
 	{
-		DrawField(window1, f, font, path);
+		DrawField(window1, f, font, path);//Draw Field of Game using function DrawField
 
 
 		while (window1.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed) window1.close();
+			if (event.type == sf::Event::Closed) window1.close();// close if press button close
 			if (event.type == sf::Event::KeyPressed)
 			{
-				if (event.key.code == sf::Keyboard::Escape) window1.close();
+				if (event.key.code == sf::Keyboard::Escape) window1.close();//close if pressing ESC
 				if (event.key.code == sf::Keyboard::Right) {
-					f.PushRight();
+					f.PushRight();//if push Right button field PushRight and place random cell 2 or 4
 					f.RndCell();
 				}
 				if (event.key.code == sf::Keyboard::Left) {
-					f.PushLeft();
+					f.PushLeft();//if push Left button field PushLeft and place random cell 2 or 4
 					f.RndCell();
 				}
 				if (event.key.code == sf::Keyboard::Up) {
-					f.PushUp();
+					f.PushUp();//if push Up button field PushUp and place random cell 2 or 4
 					f.RndCell();
 				}
 				if (event.key.code == sf::Keyboard::Down) {
-					f.PushDown();
+					f.PushDown();//if push Down button field PushDown and place random cell 2 or 4
 					f.RndCell();
 				}
 				if (event.key.code == sf::Keyboard::F1) {
-					Save(f, path);
+					Save(f, path);//if push F1 than game field save to txt file and game close
 					window1.close();
 				}
 			}
 			
 		}
 
-		window1.display();
+		window1.display();//upload window
 		window1.clear();
 	}
 
 	sf::RenderWindow window2(sf::VideoMode(600, 600), "1024");
 	window2.setFramerateLimit(60);
 
+	/*If User passed this game (has cell with value 1024) he will see this screen*/
 	while ((window2.isOpen()) && (f.CheckWin()))
 	{
-		window2.clear();
+		window2.clear();//upload window
 
 		sprite.setPosition(sf::Vector2f(0, 0));
 		sprite.setScale(sf::Vector2f(1.f, 2.f));
-		window2.draw(sprite);
+		window2.draw(sprite);//draw texture with 1024 game
 
-		sf::Text text;
+		sf::Text text;//draw text Level Passed
 		text.setFont(font);
 		text.setString(L"Level Passed!");
 		text.setCharacterSize(50);
@@ -228,7 +243,7 @@ int main(int argc, char** argv)
 		text.move(135, 150);
 		window2.draw(text);
 
-		while (window2.pollEvent(event))
+		while (window2.pollEvent(event))//wait for event to close window
 		{
 			if (event.type == sf::Event::Closed)
 			{
@@ -240,7 +255,7 @@ int main(int argc, char** argv)
 				if (event.key.code == sf::Keyboard::Escape) window2.close();
 			}
 		}
-		window2.display();
+		window2.display();//upload window
 	}
 
 	return 0;
